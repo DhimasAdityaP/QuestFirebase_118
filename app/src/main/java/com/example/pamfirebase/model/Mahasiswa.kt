583088d304a8bf6e9d74c40d556e38cf04bddae5
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-13 10:21:30 +0700

    add data class Mahasiswa

--- app/src/main/java/com/example/pamfirebase/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pamfirebase/model/Mahasiswa.kt
@@ -1,3 +1,10 @@
 package com.example.pamfirebase.model
 
-
+data class Mahasiswa(
+    val nim:String,
+    val nama:String,
+    val alamat:String,
+    val gender:String,
+    val kelas:String,
+    val angkatan:String
+)
