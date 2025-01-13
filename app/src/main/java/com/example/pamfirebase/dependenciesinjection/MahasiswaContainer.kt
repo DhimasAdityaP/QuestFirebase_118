92118d5c723bad161cbd0a7ab19b0ca6f4db8687
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-13 10:29:25 +0700

    membuat MahasiswaContainer.kt

--- app/src/main/java/com/example/pamfirebase/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pamfirebase/dependenciesinjection/MahasiswaContainer.kt
@@ -1,1 +1,19 @@
 package com.example.pamfirebase.dependenciesinjection
+
+import com.example.pamfirebase.MahasiswaApp
+import com.example.pamfirebase.repository.NetworkRepositoryMhs
+import com.example.pamfirebase.repository.RepositoryMhs
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+
+interface InterfaceContainerApp {
+    val repositoryMhs: RepositoryMhs
+}
+
+class ContainerApp(private val context: MahasiswaApp): InterfaceContainerApp {
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    override val repositoryMhs: RepositoryMhs by lazy {
+        NetworkRepositoryMhs(firestore)
+    }
+}
