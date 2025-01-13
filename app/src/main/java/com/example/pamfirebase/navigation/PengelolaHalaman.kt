7ef389b0b411d0cb6d3fb8d7a7df0e45c4a6214b
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-13 10:55:07 +0700

    membuat PengelolaHalaman

--- app/src/main/java/com/example/pamfirebase/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pamfirebase/navigation/PengelolaHalaman.kt
@@ -1,3 +1,35 @@
 package com.example.pamfirebase.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pamfirebase.ui.theme.pages.HomeScreen
+import com.example.pamfirebase.ui.theme.pages.InsertMhsView
 
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost (
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen (
+                navigateToItemEntry = { navController.navigate(DestinasiInsert.route) }
+            )
+        }
+
+        composable(route = DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
+}
