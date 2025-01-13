74571881daca5bb7c8fe17374e89af7791c8d2aa
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-13 10:30:10 +0700

    membuat MahasiswaApp

--- app/src/main/java/com/example/pamfirebase/MahasiswaApp.kt
+++ app/src/main/java/com/example/pamfirebase/MahasiswaApp.kt
@@ -1,3 +1,13 @@
 package com.example.pamfirebase
 
+import android.app.Application
+import com.example.pamfirebase.dependenciesinjection.ContainerApp
+
+class MahasiswaApp : Application() {
+    lateinit var containerApp: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
+}
 
