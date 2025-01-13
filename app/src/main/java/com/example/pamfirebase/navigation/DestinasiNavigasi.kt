c0d9dd1f74a2fc5d679706487a83aab032b0114e
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-13 10:54:26 +0700

    membuat DestinasiNavigasi

--- app/src/main/java/com/example/pamfirebase/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pamfirebase/navigation/DestinasiNavigasi.kt
@@ -1,3 +1,17 @@
 package com.example.pamfirebase.navigation
 
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
+
+object DestinasiInsert: DestinasiNavigasi {
+    override val route = "insert"
+    override val titleRes = "Insert"
+}
 
